bd77efa0e6765f8c5152aaa453e491f4260fb33b
Hayleigh Thompson <me@hayleigh.dev>
2023-08-20 14:38:43 +0100

    :wrench: Add vite-plugin-gh-pages and configure deploys on build.

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,5 +1,14 @@
+import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
 import gleam from "vite-gleam";
 
-export default {
-  plugins: [gleam()],
-};
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/gleam-lustre/" : "/",
+  plugins: [
+    gleam(),
+    ghPages({
+      branch: "docs",
+      message: "🚀 Deploy to gh-pages.",
+    }),
+  ],
+}));
