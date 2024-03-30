ad707c0223f8e96d25bb36d2e0f5059ffd654156
Hayleigh Thompson <me@hayleigh.dev>
2024-03-30 23:02:18 +0000

    :package: Generate server component runtime.

--- priv/templates/component-entry.mjs
+++
@@ -1,4 +0,0 @@
-import { register } from '../dev/javascript/lustre/client-component.ffi.mjs';
-import { name, {component_name} as component } from '../dev/javascript/{app_name}/{module_path}.mjs';
-
-register(component(), name);
--- priv/templates/entry-with-main.mjs
+++
@@ -1,3 +0,0 @@
-import { main } from "../dev/javascript/{app_name}/{app_name}.mjs";
-
-main();
